47572508cf356d9a9b216d31321de82ad904241f
Feng Zhe <henryzhefeng@gmail.com>
2014-12-22 18:59:28 +0800

    initial

--- src/com/henryzhefeng/stopwatch/MainActivity.java
+++ src/com/henryzhefeng/stopwatch/MainActivity.java
@@ -2,6 +2,7 @@ package com.henryzhefeng.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     /**
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
