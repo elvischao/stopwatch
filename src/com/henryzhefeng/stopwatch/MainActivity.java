d2c767ea010a80a71f6bcf16e0bd06b83b05cb17
Feng Zhe <henryzhefeng@gmail.com>
2014-12-24 21:13:25 +0800

    Add button bar

--- src/com/henryzhefeng/stopwatch/MainActivity.java
+++ src/com/henryzhefeng/stopwatch/MainActivity.java
@@ -2,10 +2,22 @@ package com.henryzhefeng.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    // the input from buttons
+    public enum INPUT {
+        START, RESET, PAUSE, LAP
+    }
+
+    private State state;
+    private InitialState initialState = new InitialState();
+    private RunningState runningState = new RunningState();
+    private PausedState pausedState = new PausedState();
+
     /**
      * Called when the activity is first created.
      */
@@ -15,7 +27,43 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
 
+        // initialize
         ListView recordList = (ListView) findViewById(R.id.resultList);
         recordList.setAdapter(new RecordListAdapter());
+
+        // set state and visibility
+        state = initialState;
+        View initLayout = findViewById(R.id.init_btn_bar);
+        initLayout.setVisibility(View.VISIBLE);
+        View runLayout = findViewById(R.id.run_btn_bar);
+        runLayout.setVisibility(View.INVISIBLE);
+        View pausedLayout = findViewById(R.id.paused_btn_bar);
+        pausedLayout.setVisibility(View.INVISIBLE);
+    }
+
+    // using State Pattern
+    private abstract class State {
+        public abstract void informInput(INPUT input);
+    }
+
+    private class InitialState extends State {
+        @Override
+        public void informInput(INPUT input) {
+            // TODO
+        }
+    }
+
+    private class RunningState extends State {
+        @Override
+        public void informInput(INPUT input) {
+            // TODO
+        }
+    }
+
+    private class PausedState extends State {
+        @Override
+        public void informInput(INPUT input) {
+            // TODO
+        }
     }
 }
--- src/com/henryzhefeng/stopwatch/RecordListAdapter.java
+++ src/com/henryzhefeng/stopwatch/RecordListAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +47,8 @@ public class RecordListAdapter extends BaseAdapter {
         } else {
             view = convertView;
         }
+        TextView textView = (TextView) view.findViewById(R.id.lab_order);
+        textView.setText(String.valueOf(position));
         return view;
     }
 }
